6ae235e3c10e67700fe28324764db8e976e4b5c9
voleynik <sw-eng-kyiv@mail.com>
2018-08-04 20:51:42 +0300

    vue tutorial Getting Started with Vue.js 2.0 in 2017 for Beginners https://www.youtube.com/watch?v=NT3OxvS7McU&t=811s

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <input v-model="message"/>
+    <br/>
+    <a :href="url">Click me</a>
+
+    <p v-if="show">Show me</p>
+    <p v-else="show">This is else</p>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: "Hello world from VO4",
+      url: 'http://www.cnn.com',
+      show: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
